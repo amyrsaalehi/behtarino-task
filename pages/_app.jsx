e4c25df2dfe9f23da58873c9b4f72d6eec5e8429
Amir Salehi <salehi.amir9@gmail.com>
2022-11-08 18:48:45 +0330

    nprogressbar added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,18 @@
-import 'styles/globals.css'
+import NextNProgress from "nextjs-progressbar";
+import "styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress
+        color="#570DF8"
+        options={{
+          showSpinner: false,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
