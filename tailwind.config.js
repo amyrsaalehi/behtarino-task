a065003baa588f874fb6dfcb16ed9c87b3530ece
Amir Salehi <salehi.amir9@gmail.com>
2022-11-08 15:26:39 +0330

    baseurl in jsconfig and package.json packages and commands added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    //  App directory is an experimental feature in Next.js now! you can uncomment it later.
+    // "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
